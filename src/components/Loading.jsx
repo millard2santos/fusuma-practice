7d3c206d6d82a46a8857f250069164d6b81fca35
millard2santos <millard2santos@gmail.com>
2023-02-27 18:00:42 +0100

    Loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Loading = () => {
+  return (
+    <div className='absolute  inline-block animate-spin'>
+        <i className="animate-spin fa-solid fa-snowflake"></i>
+        <i className="animate-spin fa-solid fa-snowflake"></i>
+        <i className="animate-spin fa-solid fa-snowflake"></i>
+        <i className="animate-spin fa-solid fa-snowflake"></i>
+        <i className="animate-spin fa-solid fa-snowflake"></i>
+    </div>
+  )
+}
